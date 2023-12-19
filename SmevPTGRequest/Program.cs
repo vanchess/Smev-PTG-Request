3397063d67a183c7a859bba1f898489769a64723
Ivan <30-02@mail.ru>
2023-12-19 16:59:40 +0500

    Добавьте файлы проекта.

---
+++ SmevPTGRequest/Program.cs
@@ -0,0 +1,19 @@
+﻿using SmevPTGRequest;
+using System.Text;
+
+TransportXml transportXml = new TransportXml();
+
+string msg = string.Empty;
+
+string curDir = Directory.GetCurrentDirectory();
+string[] files = Directory.GetFiles(curDir, "Request.xml");
+foreach (string file in files)
+{
+    using (StreamReader reader = new StreamReader(file, Encoding.UTF8))
+    {
+        msg = reader.ReadToEnd();
+    }
+    msg = msg.Replace(@"<?xml version=""1.0"" encoding=""UTF-8""?>", "");
+    string guid = Guid.NewGuid().ToString();
+    transportXml.CreateMessage(guid, msg, Path.Combine(curDir, guid + ".xml"));
+}
---
+++ SmevPTGRequest/TransportXml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmevPTGRequest
+{
+    internal class TransportXml
+    {
+        private string UprmesTemplate { get; set; }
+
+        public TransportXml() 
+        {
+            using (StreamReader reader = new StreamReader(@"D:\SMEV\SmevPTG\TransportTemplate.xml", Encoding.UTF8))
+            {
+                UprmesTemplate = reader.ReadToEnd();
+            }
+        }
+
+        public void CreateMessage(string guidMessage, string xmlMessage, string filePath)
+        {
+            string result = UprmesTemplate.Replace(@"{{guidMessage}}", guidMessage)
+                                                .Replace(@"{{xmlMessage}}", xmlMessage);
+
+            Encoding utf8WithoutBom = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+            using (StreamWriter file = new StreamWriter(filePath, false, utf8WithoutBom))
+            {
+                file.Write(result);
+            }
+        }
+
+    }
+}
